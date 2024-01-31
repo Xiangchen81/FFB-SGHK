d5879c00f653c180e8575aa9869b014ad7fd4da2
Xiangchen <xiangchen0709@gmail.com>
2024-01-31 10:46:40 +0800

    add js folder

---
+++ js01/grade.js
@@ -0,0 +1,6 @@
+let score = prompt("please input score");
+if (score>=90) {console.log("grade =\"A\"");}
+else if(score>=80 && score<90) {console.log("grade =\"B\"");}
+else if(score>=70 && score<80) {console.log("grade =\"C\"");}
+else if(score>=60 && score<70) {console.log("grade =\"D\"");}
+else {console.log("grade =\"F\"");}
---
+++ js01/printnumber.js
@@ -0,0 +1,4 @@
+let start_number = prompt("please input start number:");
+let end_number = prompt("please input end number:");
+for (let n = start_number; n <= end_number; n++)
+{console.log(n);}
